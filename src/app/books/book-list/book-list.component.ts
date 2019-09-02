732583ea1a46963fccdb8e5fec54aaaa1aa22630
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-09-02 09:30:31 +0200

    angular exam

---
+++ src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+import { Book } from "../book.model";
+import { Newspaper } from "../newspaper.model";
+import { BookService } from "../book.service";
+
+@Component({
+  selector: "app-book-list",
+  templateUrl: "./book-list.component.html",
+  styleUrls: ["./book-list.component.css"]
+})
+export class BookListComponent implements OnInit {
+  private books = [
+    new Book("Stars from Eger", "The revenge of Jumurdzsák"),
+    new Book("Boys from Pál street", "RIP Nemecsek"),
+    new Book("Sons of a man with stone heart", "")
+  ];
+
+  private newspapers = [
+    new Newspaper("Blikk"),
+    new Newspaper("Népszabadság"),
+    new Newspaper("Magyar Nemzet")
+  ];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit() {
+    this.bookService.getBooks().subscribe((data: Book[]) => {
+      this.books = data;
+    });
+  }
+}
